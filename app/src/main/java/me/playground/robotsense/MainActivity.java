4987800e6e76a06a81affa9798f5ada5e4ff48c1
Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>
2019-06-08 18:49:36 +0200

    UI improvements
    1. Force landscape orientation
    2. Add a toolbar
    3. Add a "Settings" menu entry to switch to the settings activity
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>

--- app/src/main/java/me/playground/robotsense/MainActivity.java
+++ app/src/main/java/me/playground/robotsense/MainActivity.java
@@ -3,11 +3,17 @@ package me.playground.robotsense;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
 
 import com.erz.joysticklibrary.JoyStick;
 
 /* Based on work of edgarramirez */
-public class MainActivity extends Activity implements JoyStick.JoyStickListener {
+public class MainActivity extends AppCompatActivity implements JoyStick.JoyStickListener {
     private String TAG = "RSense";
     private Sensor sensor;
 
@@ -28,6 +34,28 @@ public class MainActivity extends Activity implements JoyStick.JoyStickListener 
         joy2.enableStayPut(true);
         joy2.setPadColor(Color.parseColor("#556fff6f"));
         joy2.setButtonColor(Color.parseColor("#55ff0000"));
+
+        Toolbar tb = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(tb);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(i);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
